da6ebd6cc01899a6d30a086462ef58f4328516f0
Charan <charanlucky6143@gmail.com>
2022-10-05 14:55:51 +0530

    added Modal

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import _ from 'lodash';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 // const StreamEdit = (props) => {
 
@@ -31,12 +33,21 @@ class StreamEdit extends React.Component {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues);
+    }
+
     render() {
         if (!this.props.stream) {
             return (<div>Loading..</div>);
         }
 
-        return (<div>{this.props.stream.title}</div>);
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit} />
+            </div>
+        );
     }
 }
 
@@ -44,4 +55,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
